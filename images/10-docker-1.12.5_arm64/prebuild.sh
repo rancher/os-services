030cdde45b1300c0bd0959070904378981a22c7a
Sven Dowideit <SvenDowideit@home.org.au>
2017-01-13 06:01:29 +0000

    docker 1.12.6
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- images/10-docker-1.12.5_arm64/prebuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-cp -r /assets/docker-1.12.5_arm64 ./images/10-docker-1.12.5_arm64/engine
---
+++ images/10-docker-1.12.6/prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+cp -r /assets/docker-1.12.6 ./images/10-docker-1.12.6/engine

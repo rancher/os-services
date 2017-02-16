d5dbe52e22ca775681c652898c9fbfcf8aaa9f23
Sven Dowideit <SvenDowideit@home.org.au>
2017-02-17 09:48:43 +1000

    Merge pull request #95 from rancher/add-mnt-to-consoles
    add /mnt and /media shared mounts to the consoles
    (cherry picked from commit cb63f6971ddb813c6477cdbcd69a31e2def7f402)

--- images/20-zfs/wonka.sh
+++ images/20-zfs/wonka.sh
@@ -5,6 +5,7 @@ exec system-docker run --rm --privileged \
                 --net host \
                 --ipc host \
 		-v /mnt:/mnt:shared \
+		-v /media:/media:shared \
 		-v /dev:/host/dev \
 		-v /run:/run \
 		zfs-tools $(basename $0) $@

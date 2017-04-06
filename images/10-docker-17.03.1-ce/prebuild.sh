0207433030af2ea83f6fee0e90a52d40ed5018e3
Sven Dowideit <SvenDowideit@home.org.au>
2017-04-06 11:41:04 +0000

    docker:17.03.1 and docker:17.04.0
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

---
+++ images/10-docker-17.03.1-ce/prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+cp -r /assets/docker-17.03.1-ce ./images/10-docker-17.03.1-ce/engine
---
+++ images/10-docker-17.04.0-ce/prebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+cp -r /assets/docker-17.04.0-ce ./images/10-docker-17.04.0-ce/engine

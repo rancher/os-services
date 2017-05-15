f513985599ea9fe23d838e2719f8543cfff55d45
Sven Dowideit <sven@y260.home.org.au>
2017-05-15 23:37:04 +1000

    Only download/build images that don't exist yet
    Signed-off-by: Sven Dowideit <sven@y260.home.org.au>

--- images/10-docker-1.10.3_arm64/prebuild.sh
+++ images/10-docker-1.10.3_arm64/prebuild.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 set -e
 
-cp -r /assets/docker-1.10.3_arm64 ./images/10-docker-1.10.3_arm64/engine
+VERSION=$1
+ARCH=$2
+if [ "$ARCH" == "amd64" ]; then
+  ARCH="x86_64"
+  URL="https://get.docker.com/builds/Linux/${ARCH}/docker-${VERSION}"
+else
+  URL="https://github.com/rancher/docker/releases/download/v${VERSION}-${ARCH}/docker-${VERSION}_${ARCH}"
+  SUFFIX="_${ARCH}"
+fi
+
+DEST="./images/10-docker-${VERSION}${SUFFIX}/engine"
+
+mkdir -p $DEST
+curl -sL ${URL} > $DEST/docker
+chmod +x $DEST/docker

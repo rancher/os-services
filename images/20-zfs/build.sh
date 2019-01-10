20a89379f106bd07a4f6a3c11a41a4022e2a0bd7
jdoucerain <jerome.doucerain@bell.ca>
2019-01-09 21:33:27 -0500

    add http proxy environment variables to zfs-tools build context (#180)
    (cherry picked from commit 0aa03e50645f187340d2cd81afb6774f13a68b2c)

--- images/20-zfs/build.sh
+++ images/20-zfs/build.sh
@@ -121,7 +121,20 @@ done
 for i in $(ls arch/sbin); do
    echo "system-docker cp wonka.sh \${1}:/sbin/$i" >> /dist/arch/setup_wonka.sh
 done
-system-docker build --network=host -t zfs-tools arch/
+
+if [ "XX$HTTP_PROXY" != "XX" ]; then
+    BUILD_ARGS="--build-arg HTTP_PROXY --build-arg http_proxy="$HTTP_PROXY
+fi
+
+if [ "XX$HTTPS_PROXY" != "XX" ]; then
+    BUILD_ARGS=$BUILD_ARGS" --build-arg HTTPS_PROXY --build-arg https_proxy="$HTTPS_PROXY
+fi
+
+if [ "XX$NO_PROXY" != "XX" ]; then
+    BUILD_ARGS=$BUILD_ARGS" --build-arg NO_PROXY --build-arg no_proxy="$NO_PROXY
+fi
+
+system-docker build --network=host $BUILD_ARGS -t zfs-tools arch/
 
 modprobe zfs
 

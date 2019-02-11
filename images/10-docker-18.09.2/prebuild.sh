274f7178e66ddef8e26c4d52d1dee221bce558c6
niusmallnan <niusmallnan@gmail.com>
2019-02-12 01:31:41 +0800

    Add docker 18.09.2

---
+++ images/10-docker-18.09.2/prebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+VERSION=$1
+ARCH=$2
+if [ "$ARCH" == "amd64" ]; then
+    DOCKERARCH="x86_64"
+    URL="https://download.docker.com/linux/static/stable/${DOCKERARCH}/docker-${VERSION}.tgz"
+    COMPLETION_URL="https://raw.githubusercontent.com/docker/cli/v${VERSION}/contrib/completion/bash/docker"
+fi
+
+DEST="./images/10-docker-${VERSION}${SUFFIX}"
+
+mkdir -p $DEST
+curl -sL ${URL} | tar xzf - -C $DEST
+curl -sL -o $DEST/docker/completion ${COMPLETION_URL} 
+mv $DEST/docker $DEST/engine

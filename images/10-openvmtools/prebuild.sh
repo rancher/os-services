e4ad7f243364975eeb586281019f6083d5337f85
Jason-ZW <zhenyang@rancher.com>
2019-05-10 14:11:56 +0800

    Bump up open-vm-tools to 10.3.10

--- images/10-openvmtools/prebuild.sh
+++ images/10-openvmtools/prebuild.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 
 rm -rf ./bin ./lib ./etc
 
-wget https://github.com/Jason-ZW/open-vm-tools/releases/download/stable-10.2.5/open-vm-tools.tar.gz \
+wget https://github.com/Jason-ZW/open-vm-tools/releases/download/stable-10.3.10/open-vm-tools.tar.gz \
 	&& tar xvzf open-vm-tools.tar.gz \
 	&& cp -r open-vm-tools-build/bin . \
 	&& cp -r open-vm-tools-build/lib . \
